671b6dd37a56a91d97b3c4985754f09e1a17a3ee
Ben D.Ry <Ben.dery@hotmail.com>
2023-09-22 09:36:01 -0400

    Fix EngineSim header constructor class.

--- EngineSimulator/EngineSimulatorMain.h
+++ EngineSimulator/EngineSimulatorMain.h
@@ -2,19 +2,21 @@
 
 #include <Windows.h>
 
-class EngineSimulator {
-public:
-    EngineSimulator(HWND hwnd);
-    void run();
+namespace EngineSimulator {
+    class EngineSimulatorMain {
+    public:
+        EngineSimulatorMain(HWND hwnd);
+        void run();
 
-private:
-    void initialize();
-    void update();
-    void render();
-    void calculate_forces();
-    void update_grid();
+    private:
+        void initialize();
+        void update();
+        void render();
+        void calculate_forces();
+        void update_grid();
 
-    HWND m_hwnd;
-    // TODO: Add DirectX 12 device and resource variables
-    // TODO: Add dataset variables
-};
+        HWND m_hwnd;
+        // TODO: Add DirectX 12 device and resource variables
+        // TODO: Add dataset variables
+    };
+}
